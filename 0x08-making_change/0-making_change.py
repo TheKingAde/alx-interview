23b81a4fcb650a90af26075f72b59d5b0b810c15
Ade <theking-ade@outlook.com>
2024-07-05 05:40:33 -0700

    [update] 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""
-fucntion determine the fewest number of coins needed
-to meet  a given amount total
-"""
+"""Making Change Problem"""
 
 
 def makeChange(coins, total):
+    """Determines the fewest number of coins needed \
+        to meet a given amount total"""
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
+    current_total = 0
+    used_coins = 0
+    coins = sorted(coins, reverse=True)
     for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+        r = (total - current_total) // coin
+        current_total += r * coin
+        used_coins += r
+        if current_total == total:
+            return used_coins
+    return -1
